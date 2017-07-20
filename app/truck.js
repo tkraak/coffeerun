28939feeba7dbaaf4865521672112352869a7b7d
Tom Kraak <tom.kraak@gmail.com>
2017-07-20 00:22:22 -0400

    add deliver and print order methods

--- app/truck.js
+++ app/truck.js
@@ -5,7 +5,22 @@
   }
 
   Truck.prototype.createOrder = function (order) {
+    console.log(`adding order for ${order.emailAddress}`)
     this.db.add(order.emailAddress, order)
+  }
+
+  Truck.prototype.deliverOrder = function (id) {
+    console.log(`delivering order for ${id}`)
+    this.db.remove(id)
+  }
+
+  Truck.prototype.printOrders = function () {
+    const customers = Object.keys(this.db.getAll())
+
+    console.log(`truck # ${this.truckId} has pending orders:`)
+    customers.forEach(id => {
+      console.log(this.db.get(id))
+    })
   }
 
   exports.Truck = Truck
