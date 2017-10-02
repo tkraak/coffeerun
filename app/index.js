ddfd065bef9a33e905810f536025ff040d757b10
Tom Kraak <tom.kraak@gmail.com>
2017-10-01 23:49:27 -0400

    destructure app namespace

--- app/index.js
+++ app/index.js
@@ -1,10 +1,7 @@
 (function (exports) {
   const form = '[data-coffee-order="form"]'
   const cl = '[data-coffee-order="checklist"]'
-  const Truck = exports.app.Truck
-  const DataStore = exports.app.DataStore
-  const FormHandler = exports.app.FormHandler
-  const CheckList = exports.app.CheckList
+  const { CheckList, DataStore, FormHandler, Truck } = exports.app
   const truck = new Truck('ncc-1701', new DataStore())
   const checkList = new CheckList(cl)
   const formHandler = new FormHandler(form)
