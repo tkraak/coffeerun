98c5770977793b8f931e2f2507fa76d35658d5af
Tom Kraak <tom.kraak@gmail.com>
2018-02-11 14:39:53 -0500

    secure api host

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 (function (exports) {
   const { Validation, CheckList, RemoteDataStore, FormHandler, Truck } = exports.app
-  const apiUrl = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders'
+  const apiUrl = 'https://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders'
   const truck = new Truck('ncc-1701', new RemoteDataStore(apiUrl))
   const checkList = new CheckList('[data-coffee-order="checklist"]')
   const formHandler = new FormHandler('[data-coffee-order="form"]')
