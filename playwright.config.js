1eddc67aab3549aa4a78f1785313891525a8dd1f
Tom Kraak <tom.kraak@gmail.com>
2026-01-12 21:36:40 -0500

    move e2e back out of test

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@
 const { defineConfig, devices } = require('@playwright/test')
 
 module.exports = defineConfig({
-  testDir: './test/e2e',
+  testDir: './e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
